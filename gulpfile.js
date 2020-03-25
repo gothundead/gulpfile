285d7932ab4621d4ea1fe8ec4aaf25fc0d061833
Mihail <r@gothundead.ru>
2020-03-25 23:11:31 +0300

    25.03.2020

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp       = require('gulp'), // Подключаем Gulp
 	sass         = require('gulp-sass'), //Подключаем Sass пакет,
+	less		= require('gulp-less'),
+	cleanCss 	= require('gulp-clean-css'),
 	browserSync  = require('browser-sync'), // Подключаем Browser Sync
 	concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
 	uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
@@ -9,7 +11,32 @@ var gulp       = require('gulp'), // Подключаем Gulp
 	imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
 	pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
 	cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
-	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
+	lessAutoprefix = require('less-plugin-autoprefix'),
+	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов;
+	autoprefixLess = new lessAutoprefix({ browsers: ['last 2 versions'] });
+
+var buildCssFile = [ // Переносим библиотеки в продакшен
+	'src/css/**/*.css',
+	'src/css_static/**/*.css'
+];
+
+
+gulp.task('css-static', function() {
+	return gulp.src('./src/css_static/**/*.css')
+		.pipe(concat('css_static.css'))
+/*		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			// browsers: ['> 0.1%'],
+			cascade: false
+		}))*/
+		.pipe(cleanCss({
+			level: 2
+		}))
+		.pipe(gulp.dest('./build/css'))
+		.pipe(browserSync.stream());
+});
+
+
 
 gulp.task('sass', function() { // Создаем таск Sass
 	return gulp.src('src/sass/**/*.sass') // Берем источник
@@ -17,6 +44,15 @@ gulp.task('sass', function() { // Создаем таск Sass
 		.pipe(autoprefixer(['last 3 versions', '> 1%'/*, 'ie 8', 'ie 7'*/], { cascade: true })) // Создаем префиксы
 		.pipe(gulp.dest('src/css')) // Выгружаем результата в папку src/css
 		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('less', function() { // Создаем таск less
+	return gulp.src('src/less/**/*.less')
+		.pipe(less({
+			plugins: [autoprefixLess]
+		}))
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении;
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
@@ -43,9 +79,19 @@ gulp.task('code', function() {
 	.pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('css-libs', function() {
-	return gulp.src('src/sass/libs.sass') // Выбираем файл для минификации
+gulp.task('css-libs-sass', function() {
+	return gulp.src('src/sass/**/*.sass') // Выбираем файл для минификации
 		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(cssnano()) // Сжимаем
+		.pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+		.pipe(gulp.dest('src/css')); // Выгружаем в папку src/css
+});
+
+gulp.task('css-libs-less', function() {
+	return gulp.src('src/less/**/*.less') // Выбираем файл для минификации
+		.pipe(less({
+			plugins: [autoprefixLess]
+		})) // Преобразуем less в CSS посредством gulp-less
 		.pipe(cssnano()) // Сжимаем
 		.pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
 		.pipe(gulp.dest('src/css')); // Выгружаем в папку src/css
@@ -69,10 +115,7 @@ gulp.task('img', function() {
 
 gulp.task('prebuild', async function() {
 
-	var buildCss = gulp.src([ // Переносим библиотеки в продакшен
-		'src/css/main.css',
-		'src/css/libs.min.css'
-		])
+	var buildCss = gulp.src(buildCssFile)
 	.pipe(gulp.dest('build/css'))
 
 	var buildFonts = gulp.src('src/fonts/**/*') // Переносим шрифты в продакшен
@@ -92,8 +135,11 @@ gulp.task('clear', function (callback) {
 
 gulp.task('watch', function() {
 	gulp.watch('src/sass/**/*.sass', gulp.parallel('sass')); // Наблюдение за sass файлами
+	gulp.watch('src/less/**/*.less', gulp.parallel('less')); // Наблюдение за less файлами
 	gulp.watch('src/*.html', gulp.parallel('code')); // Наблюдение за HTML файлами в корне проекта
-	gulp.watch(['src/js/common.js', 'src/libs/**/*.js'], gulp.parallel('scripts')); // Наблюдение за главным JS файлом и за библиотеками
+	// gulp.watch(['src/js/common.js', 'src/libs/**/*.js'], gulp.parallel('scripts')); // Наблюдение за главным JS файлом и за библиотеками
+	gulp.watch(['src/libs/**/*.js'], gulp.parallel('scripts')); // Наблюдение за главным JS файлом и за библиотеками
+	gulp.watch(['src/css_static/**/*.css'], gulp.parallel('css-static')); // Наблюдение статичными css-файлами
 });
-gulp.task('default', gulp.parallel('css-libs', 'sass', 'scripts', 'browser-sync', 'watch'));
-gulp.task('build', gulp.parallel('prebuild', 'clean', 'img', 'sass', 'scripts'));
+gulp.task('default', gulp.parallel('css-libs-sass', 'css-libs-less', 'sass', 'less', 'scripts', 'css-static', 'browser-sync', 'watch'));
+gulp.task('build', gulp.parallel('prebuild', 'clean', 'img', 'sass', 'less', 'scripts', 'css-static'));
